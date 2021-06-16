1eed963bc0e77adb4b4dc34a9b9406049e711f57
Ying <ying@stucomm.com>
2021-06-16 14:09:44 +0200

    Add Kimiko

---
+++ keyboards/keycapsss/kimiko/keymaps/ykz89/rules.mk
@@ -0,0 +1,12 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = no        # ENables the use of one or more encoders
+RGB_MATRIX_ENABLE = yes	   # Use RGB Matrix
+NKRO_ENABLE = yes		   # Enables NKRO
+# LTO: Link Time Optimizations.
+# Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
+# This does not affect QMK Macros and Layers
+LTO_ENABLE = yes
+
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = yes # Auto Shift
